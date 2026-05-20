7615b0d9455e489fb921dc73aeb01f7c98a7fb18
Pamarthi Sri Lakshmi <srilakshmipamarthi076@gmail.com>
2026-05-20 14:05:56 +0530

    fixed server error

--- smart_mail_assistant/mailapp/views.py
+++ smart_mail_assistant/mailapp/views.py
@@ -1,12 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
-from .gmail_reader import read_emails
-
+from django.http import HttpResponse
 
 def home(request):
-    mails = read_emails()
-
-    return render(request, 'home.html', {
-        'mails': mails
-    })
+    return HttpResponse("Smart Mail Reminder Project Running Successfully 🚀")
